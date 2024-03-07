aa458afb77819464e459f7e7be9e26688f7cc3bb
Dmitriy <66624150+alferovde@users.noreply.github.com>
2024-03-07 11:08:08 +0300

    add fonts

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import SVGimage from "../../MyComponents/SVGimage/SVGimage";
+import { logo } from "../../Store/sprite";
+import "./header.scss";
+import MyButton from "../../MyComponents/MyButton/MyButton";
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__wrapper container">haedar</div>
+      <div className="header__wrapper container">
+        <SVGimage src={logo} />
+        haedar
+        <MyButton> Get Started </MyButton>
+      </div>
     </header>
   );
 };
---
+++ src/MyComponents/SVGimage/SVGimage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SVGimage = ({ src }) => {
+  return <>{src}</>;
+};
+
+export default SVGimage;
