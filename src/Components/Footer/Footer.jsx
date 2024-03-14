bce7caf09d8f09a6c111f36510e844b736ce7b45
Dmitriy <66624150+alferovde@users.noreply.github.com>
2024-03-14 10:49:34 +0300

    start login modal

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 import "./footer.scss";
+import SVGimage from "../../MyComponents/SVGimage/SVGimage";
+import { logo } from "../../Store/sprite";
+import Menu from "../Menu/Menu";
+import { NavLink } from "react-router-dom";
+import { socialItems } from "../../Store/store";
 const Footer = () => {
+  const renderSocial = () => {
+    return socialItems.map((item) => {
+      return (
+        <li key={item.id}>
+          <SVGimage src={item.img} />
+        </li>
+      );
+    });
+  };
+
   return (
     <footer className="footer">
-      <div className="footer__wrapper container">Footer</div>
+      <div className="footer__wrapper container">
+        <div className="footer__top">
+          <NavLink to={"/"}>
+            <SVGimage src={logo} />
+          </NavLink>
+
+          <Menu />
+        </div>
+        <div className="footer__bottom">
+          <div className="rights">© 2023 EATLY All Rights Reserved.</div>
+          <div className="social">
+            <ul>{renderSocial()}</ul>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./loginform.scss";
+const LoginForm = () => {
+  return <div className="login-modal__continer">LoginForm</div>;
+};
+
+export default LoginForm;
